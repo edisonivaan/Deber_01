29ed0ff813537be46aabb6d58adc30b7d3659bf0
edisonivaan <edisonleo0000@gmail.com>
2022-12-07 20:53:59 -0500

    actualizacion

--- src/Escalera.java
+++ src/Escalera.java
@@ -1,6 +1,24 @@
 import java.util.Scanner;
-
 public class Escalera {
+
+    /**
+     * Este método permite crear una escalera con un número de escalones
+     * ingresado por consola
+     * @param numEsca - número ingresado por consola
+     */
+    public static void dibujarEscalera(int numEsca) {
+        //Este for permite dibujar los caracteres el número de veces que indique el número ingresado por consola
+        for(int i = 0 ; i < numEsca ; i++ ){
+            //Este for permite crear los espacios debajo de las líneas horizontales
+            for(int j = 0 ; j < i ; j++ )
+                System.out.print("   ");
+            //Este if permite escribir la linea vertical después de escribir las primeras líneas horizontales
+            if(i>0)
+                System.out.print("┤");
+            System.out.println("__");
+        }
+    }
+
     public static void main(String[] args) {
 
         //Definición de variables y Scanner
@@ -10,14 +28,7 @@ public class Escalera {
         System.out.println("Digite la cantidad de escalones que desea que tenga la escalera");
         int numEsca = sc.nextInt();
 
-        for(int i=0; i<numEsca; i++){
-            for(int j=0; j<i;j++)
-                System.out.print("   ");
-            if(i>0)
-                System.out.print("┤");
-            System.out.println("__");
-        }   
-
-
+        //Método para dibujar la escalera con caracteres
+        dibujarEscalera(numEsca);
     }
 }
